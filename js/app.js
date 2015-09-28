4ffdf92ef5a06028b72da8b1570c813cc789303d
kedarjoyner <kedarjoyner@gmail.com>
2015-09-28 12:28:14 -0500

    shopping app

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/* This is my interactive shopping 
+list app developed as an assignment 
+through Thinkful.com. My shopping
+list app allows users to add items
+to a list, and check them off or 
+delete them as they aquire 
+each item. */
+
+$(document).ready(function() {
+  alert('Oh, hi there, world');
+});
