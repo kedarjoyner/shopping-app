f1641db3ce9b0f6cd1a2cda07b07c8ef4447c91a
kedarjoyner <kedarjoyner@gmail.com>
2015-09-30 14:22:25 -0500

    jQuery mentor session

--- js/app.js
+++ js/app.js
@@ -7,5 +7,30 @@ delete them as they aquire
 each item. */
 
 $(document).ready(function() {
-	$('.form-submit').on('click', enterItem);
+	/*$('.form-submit').on('click', function(event) {
+		var input = ()*/
+
+	$('.reset-button').click(function() {
+		//alert('Test');
+		//$('.items').empty(); //empty's all children
+		$('li').remove(); //removes all classes that match li
+	});
+
+	$('.form-submit').click(function() {
+		//alert('Test');
+		var input = $('.enter-item').val();//current value that user inputed in text box
+		$('.items').prepend('<li>' + input + '<span class="icons"><a href="#" class="unchecked-box"><i class="fa fa-square-o"></i></a><a href="#" class="delete-circle"><i class="fa fa-minus-circle"></i></a></span></li>');
+		$('.enter-item').val('');//setting input to blank
+	});
+
+	$('.items').on('click', '.delete-circle', function() { 
+		//alert('test');
+		$(this).closest('li').remove(); //find 'this' object. find closest list element
+	});
+
+	$('.items').on('click', '.unchecked-box', function() {
+		//alert('test');
+
+	});
 })
+
